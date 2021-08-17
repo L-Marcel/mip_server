ed3b16c8f4e92aff50bd2bec81f568eae6014f51
C-Fernandes <66803310+C-Fernandes@users.noreply.github.com>
2021-08-17 18:12:30 -0300

    Verificação de dados do usuario

---
+++ src/validations/user.ts
@@ -0,0 +1,26 @@
+
+import Joi from 'joi';
+
+export default class UserValidation {
+    static check(reqType?: "create" | "update") {
+        let rt = reqType !== undefined ? reqType : "create";
+
+        return Joi.object().keys({
+            id: Joi.number().min(1).integer().custom((value, helper) => {
+                if (rt === "update" && value === undefined) {
+                    return helper.error("any.required");
+                } else if (rt === "create" && value !== undefined) {
+                    return helper.error("any.unknown");
+                } else {
+                    return value;
+                };
+            }),
+            name: Joi.string().required().min(4).max(30),
+            email: Joi.string().required().min(10).max(50),
+            password: Joi.string().required().min(8),
+            phone: Joi.string().required().min(10).max(14),
+
+        });
+    };
+};
+
